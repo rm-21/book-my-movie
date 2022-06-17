ea1b1809b7dc72ab1d56345a536a47df7cb610bd
rm-21 <mittalrishabh181@gmail.com>
2022-06-17 23:17:15 +0530

    Displayed home page

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Home from "../screens/home/Home";
 import Details from "../screens/details/Details";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
 
 const Controller = () => {
-  const baseUrl = "/api/v1/";
+  const baseUrl = "http://localhost:8085/api/v1/";
 
   return (
     <Router>
---
+++ src/screens/home/Home.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+function Home() {
+  return <div>Hello World!</div>;
+}
+
+export default Home;
