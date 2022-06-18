f4896cb2d366e555ad762f4691d11de2d317a0ae
rm-21 <mittalrishabh181@gmail.com>
2022-06-18 16:24:52 +0530

    Added axios and useEffect to fetch data

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect, Fragment } from "react";
 import Header from "../../common/header/Header";
 import Heading from "../../common/heading/Heading";
+import axios from "axios";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8085/api/v1/movies")
+      .then((response) => setMovies(response.data.movies));
+
+    axios
+      .get("http://localhost:8085/api/v1/genres")
+      .then((response) => setGenres(response.data.genres));
+
+    axios
+      .get("http://localhost:8085/api/v1/artists")
+      .then((response) => setArtists(response.data.artists));
+  }, []);
+
   return (
     <Fragment>
-      <Header />
+      <Header bookShow={false} />
       <Heading />
     </Fragment>
   );
