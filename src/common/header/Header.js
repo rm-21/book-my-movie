2501c77eef01d6af77489df222f78ce429b0d186
rm-21 <mittalrishabh181@gmail.com>
2022-06-18 15:39:45 +0530

    Added Login and Register tabs in Modal

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -16,32 +16,11 @@ import {
 import Logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import { makeStyles } from "@material-ui/core/styles";
-import { flexbox } from "@mui/system";
 
 export default function Header({ bookShow }) {
   // states
   const [loggedIn, setLoggedIn] = useState(false);
   const [loginModal, setLoginModal] = useState(false);
-
-  // makeStyles gets accesses to the global themes
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      display: "flex",
-      flexDirection: "column",
-      alignContent: "nowrap",
-      justifyContent: "center",
-    },
-
-    button: {
-      marginTop: theme.spacing(4),
-      marginBottom: theme.spacing(1),
-      marginRight: theme.spacing(8),
-      marginLeft: theme.spacing(8),
-    },
-  }));
-
-  const classes = useStyles();
 
   const handleLoginClick = () => {
     setLoggedIn(true);
@@ -76,7 +55,12 @@ export default function Header({ bookShow }) {
         )}
       </div>
 
-      {loginModal && <LoginModal handleModalSubmit={handleModalSubmit} />}
+      {loginModal && (
+        <LoginModal
+          handleModalSubmit={handleModalSubmit}
+          loginModal={loginModal}
+        ></LoginModal>
+      )}
     </div>
   );
 }
