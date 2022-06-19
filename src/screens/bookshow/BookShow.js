f9f4d40f5c3e77a539bbb2aca662a3f39ac10c86
rm-21 <mittalrishabh181@gmail.com>
2022-06-19 12:33:21 +0530

    Updated total price in bookshow.js

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -285,9 +285,7 @@ const BookShow = (props) => {
             <br />
             <Typography>Unit Price: Rs. {unitPrice}</Typography>
             <br />
-            <Typography>
-              Total Price: Rs. {unitPrice * tickets}
-            </Typography>
+            <Typography>Total Price: Rs. {unitPrice * tickets}</Typography>
             <br />
             <br />
             <Button
