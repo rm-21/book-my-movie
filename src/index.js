4c1386f30e1295ee62715b82ca11b454963ea714
rm-21 <mittalrishabh181@gmail.com>
2022-06-19 12:39:26 +0530

    index,js props issue fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import Controller from './screens/Controller';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "typeface-roboto";
+import registerServiceWorker from "./registerServiceWorker";
+import Controller from "./screens/Controller";
 
-ReactDOM.render(<Controller />, document.getElementById('root'));
+ReactDOM.render(<Controller />, document.getElementById("root"));
 registerServiceWorker();
